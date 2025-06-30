e644dae5a279e562710121980e025c93d0f776fb
Santosh Patapati <sapatapati@gmail.com>
2025-06-30 12:33:42 -0500

    got literally everything working! now need to prepare for vercel deployment

--- santosh_website/santosh_website/urls.py
+++ santosh_website/santosh_website/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def redirect_to_home(request, path=''):
+    """Redirect any unmatched URL to the home page"""
+    return redirect('portfolio:home', permanent=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    # Catch-all pattern - this should be last
+    re_path(r'^.*/$', redirect_to_home),
 ]
 
 handler404 = 'portfolio.views.custom_404'
