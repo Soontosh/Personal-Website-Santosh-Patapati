bc9ddeb78dc8ba7098863c524a11969073104946
Santosh Patapati <sapatapati@gmail.com>
2025-06-30 12:07:09 -0500

    simple portfolio http response

--- santosh_website/portfolio/views.py
+++ santosh_website/portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome to my portfolio!")
+
+def custom_404(request, exception):
+    """Custom 404 error handler"""
+    return HttpResponseNotFound("Page not found")
