38721c4e627fb3717b78f63e9ede9afa35810894
Santosh Patapati <sapatapatiwork@gmail.com>
2025-06-30 21:44:16 -0500

    changed static path

--- santosh_website/settings.py
+++ santosh_website/settings.py
@@ -133,14 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio', 'static'),
 ]
 
+# Use consistent static root for Vercel
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+
+# WhiteNoise configuration for better static file serving
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
